41a46d8bc9ceffefed8128892e54f4a182751079
Muhammad Rifandy Syahermi <107825688+Rifandy17@users.noreply.github.com>
2022-06-20 20:03:01 +0700

    Tugas-Praktikum-1-C ++
    Tugas-Praktikum-1-C ++

---
+++ Praktikum 1  C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+string salam1 = "Assalamualaikum, ";
+string nama_saya;
+int angka1 = 6;
+int angka2 = 4;
+int tambah = angka1 + angka2;
+
+int main(){
+	cout<<"Nama Kamu : ";
+	cin>>nama_saya;
+	cout<<salam1<<nama_saya<<endl;
+	cout<<"Angka adalah : "<<angka1<<"\n";
+	cout<<"Angka 2 adalah : "<<angka2<<"\n";
+	cout<<"Hasil tambah : "<<tambah;
+}
+
